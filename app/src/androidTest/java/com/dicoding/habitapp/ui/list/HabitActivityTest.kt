5215433845ccdfed2dafc66df2730f602184b355
muhfrds345@gmail.com <muhfrds345@gmail.com>
2023-12-09 22:52:18 +0700

    Initial commit completed all Todo and become a good app Habit

---
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -0,0 +1,36 @@
+package com.dicoding.habitapp.ui.list
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HabitActivityTest {
+
+    @get:Rule
+    val intentsRule = IntentsTestRule(HabitListActivity::class.java)
+
+    @Test
+    fun testAddHabitActivityDisaplayed() {
+        onView(withId(R.id.fab)).perform(click())
+
+        intended(hasComponent(AddHabitActivity::class.java.name))
+
+        onView(withId(R.id.add_ed_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_ed_minutes_focus)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_tv_start_time)).check(matches(isDisplayed()))
+    }
+}
